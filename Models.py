e6e9549d7fa6b72519746352a5b8126fce8fd6a0
sanjay <44315700+sanjumsanthosh@users.noreply.github.com>
2023-12-10 00:56:13 +0530

    Pushing the model and onnx

--- Models.py
+++ Models.py
@@ -6,13 +6,13 @@ class LeNet(nn.Module):
 
   def __init__(self):
     super(LeNet, self).__init__() 
-    self.conv1 = nn.Conv2d(1,6,5) 
-    self.pool1 = nn.MaxPool2d(2,2) 
-    self.conv2 = nn.Conv2d(6,16,5) 
-    self.pool2 = nn.MaxPool2d(2,2) 
-    self.fc1 = nn.Linear(16*4*4, 120)
-    self.fc2 = nn.Linear(120,84)
-    self.fc3 = nn.Linear(84,10) 
+    self.conv1 = nn.Conv2d(1,6,5) # kernel 5 and sride 1 => 28X28
+    self.pool1 = nn.MaxPool2d(2,2) # kernel 2 and sride 2 => 24X24
+    self.conv2 = nn.Conv2d(6,16,5) # kernel 5 and sride 1 => 12X12
+    self.pool2 = nn.MaxPool2d(2,2) # kernel 2 and sride 2 => 8X8 
+    self.fc1 = nn.Linear(16*4*4, 120) # 4x4 feature map
+    self.fc2 = nn.Linear(120,84) # 120 input and 84 output
+    self.fc3 = nn.Linear(84,10) # 84 input and 10 output
 
 
   def forward(self,x):
--- ONNX-converter.py
+++ ONNX-converter.py
@@ -4,7 +4,7 @@ from Models import LeNet
 
 torch.onnx.export(
     LeNet().eval(),
-    torch.randn(1, 3, 512, 512),
+    torch.randn(1, 1, 28, 28),
     "lenet.onnx",
     input_names=["input"],
     output_names=["output"],
